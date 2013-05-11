f188386a8276a622733e2958486f6863df5b4fd5
Damon Oehlman <damon.oehlman@sidelab.com>
2013-05-11 23:43:05 +1000

    Initial commit

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ module.exports = function() {
     return through(
         function write(data) {
             console.log(data);
+
+            this.queue(data);
         },
 
         function end() {
